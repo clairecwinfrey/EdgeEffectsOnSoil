fb5d463d2799c8102bf7922e08ed63783f7c20a1
Claire_C_Winfrey <clairecwinfrey@gmail.com>
2021-09-27 15:09:39 -0600

    Added in ubiquity = 15 for EU 52, reincorporated 50m, cleaned up a few things

--- IdentifyEdgeEffectTaxa.R
+++ IdentifyEdgeEffectTaxa.R
@@ -100,6 +100,7 @@ dim(ASVwithCount_52)
 barplot(table(ASVwithCount_52[,39]), ylab="Number of ASVs", xlab= "Number of Samples Present in (out of 38)", main= "EU_52" )
 length(which(ASVwithCount_52[,39] >= 4)) #7,514 ASVs are found in at least four samples!
 length(which(ASVwithCount_52[,39] >= 10)) #3506 ASVs found in at least 10
+length(which(ASVwithCount_52[,39] >= 15)) #1878 ASVs found in at least 15!
 length(which(ASVwithCount_52[,39] >= 20)) #1062
 length(which(ASVwithCount_52[,39] >= 30)) #310
 length(which(ASVwithCount_52[,39] >= 38)) #22 ASVs found in ALL samples!
@@ -153,8 +154,6 @@ forest_sum <- rowSums(ASVtab_52_4x_da_counts[,forest_index]) #gets sum across ea
 forest_meanAbund <- forest_sum/length(forest_index) #average number in each forest sample
 patch_sum <- rowSums(ASVtab_52_4x_da_counts[,patch_index])
 patch_meanAbund <- patch_sum/length(patch_index)
-
-# Do they differ among transects?
 
 # Combine it all together
 ASVtab_52_4x_da_counts <- cbind(ASVtab_52_4x_da_counts, forest_meanAbund, patch_meanAbund)
@@ -281,6 +280,126 @@ ggplot(relAbund_52_10x_ByMeter_gg, aes(Meter, ASV_Rel_Abundance, color = Phylum,
   geom_line() + theme(axis.text.y = element_blank()) + ggtitle("Changes in ASV abundance across EU 52 (combined by meter)")
 
 
+########################################################################
+################ ASVs THAT OCCUR AT LEAST 15 TIMES ###### 
+##               (i.e. 75% of samples in patch or forest
+## Based on conversation with Noah on September 21, 2021
+########################################################################
+######### BY WHOLE EU/SITE #######
+# Here, I do a relative abundance analysis for the WHOLE site. In other words, I combine samples based
+# on where they fall along the transect. In the next section, I leave the samples as individual samples
+# and then look by transect. 
+
+names52_15 <- names(which(ASVwithCount_52[,39] >= 15)) #this gives the names of the ASVs that occur at least
+# 15 times
+length(names52_15) #1878 matches calculation higher up
+names52_15
+
+# Remove all of the ASVs from the phyloseq object that are not in at least fifteen samples
+EU_52_15times.ps <- prune_taxa(names52_15, EU_52_Soils.ps) 
+EU_52_15times.ps #1878 
+
+# Combine samples by meter (values across ASV table is SUM across all samples at the same meter point,
+# and the rest of the metadata variables are the mean for each position along the meter)
+EU_52_15_ByMeter.ps <- merge_samples(EU_52_15times.ps, group= "Meter")
+EU_52_15_ByMeterASV <- as.data.frame(t(ASVs_outta_ps(EU_52_15_ByMeter.ps))) #ASVs are rows, meter/samples are columns
+dim(EU_52_15_ByMeterASV) #ASVs are rows, meter/samples are columns, and we have the expected 1,878 taxa
+# View(EU_52_15_ByMeterASV)
+
+# Add back in habitat information (merging samples above finds mean of the metadata variables,
+# and since habitat is categorical it goes to NAs)
+sample_data(EU_52_15_ByMeter.ps)$Habitat <- c(rep("Patch", 4), "Edge", rep("Forest",5)) 
+#ordered by meter, so 10-40 should be patch, 60-100 are forest
+sample_data(EU_52_15_ByMeter.ps)$Habitat 
+
+# Remove "edge" samples so that we can find samples that vary in edge versus forest in the differential abundance analysis
+EU_52_15timesByMeterNoEdge.ps <- subset_samples(EU_52_15_ByMeter.ps, Habitat != "Edge")
+# (not possible to have ASVs that were only on edge because had to be found in at least 15!!)
+
+# DIFFERENTIAL ABUNDANCE ANALYSIS
+Deseq1_52_15x_ByMeter <- phyloseq_to_deseq2(EU_52_15timesByMeterNoEdge.ps, ~ Habitat)
+# Note: uses default Benjamini-Hochberg correction 
+Deseqtested_52_15x_ByMeter <- DESeq(Deseq1_52_15x_ByMeter, test="Wald", fitType = "parametric")
+DeSeq_res_52_15x_ByMeter <- results(Deseqtested_52_15x_ByMeter, cooksCutoff = FALSE)
+alpha <- 0.001
+sigtab_52_15x_ByMeter <- DeSeq_res_52_15x_ByMeter[which(DeSeq_res_52_15x_ByMeter$padj < alpha), ]
+sigtab_52_15x_ByMeter <- cbind(as(sigtab_52_15x_ByMeter, "data.frame"), as(tax_table(EU_52_15timesByMeterNoEdge.ps)[rownames(sigtab_52_15x_ByMeter), ], "matrix"))
+head(sigtab_52_15x_ByMeter)
+dim(sigtab_52_15x_ByMeter) #88 ASVs out of the 1878 tested had a corrected p-value of less than 0.01. The proportion of "significant" ASVs likely goes 
+# up compared with lower ubiquity thresholds because of less testing (i.e. less of an effect of post-hoc correction for multiple testing)
+# If alpha is lowered to 0.001, there are only 35 "significant" ASVs
+# View(sigtab_52_15x_ByMeter)
+
+# How many samples are each of these ASVs found in and what is their overall abundance? (Add edge samples back in in this chunk)
+# MAXIMUM number is 9 samples because I combined earlier by meter. 
+names_52_15x_ByMeter <- rownames(sigtab_52_15x_ByMeter) #pull out names of the ASVs
+# Get ASV table (out of phyloseq) for all points along transect:
+EU_52_15_ByMeter_ASVs <- as.data.frame(t(ASVs_outta_ps(EU_52_15_ByMeter.ps))) #transpose so that ASVs are rows
+ASVtab_52_15x_ByMeter_da <- EU_52_15_ByMeter_ASVs[names_52_15x_ByMeter,] #get a smaller version of the ASV table that has only the diff abundance ASVs
+SampleCount_52_15x_ByMeter_da <- rowSums(ASVtab_52_15x_ByMeter_da > 0) #get number of soil samples that the ASV appears in
+ASVtab_52_15x_ByMeter_da_counts <- cbind(ASVtab_52_15x_ByMeter_da, SampleCount_52_15x_ByMeter_da)
+ASVabund <- rowSums(ASVtab_52_15x_ByMeter_da_counts[,1:10]) #get abundance of each ASV ACROSS all samples
+ASVtab_52_15x_ByMeter_da_counts <- cbind(ASVtab_52_15x_ByMeter_da_counts, ASVabund)
+#View(ASVtab_52_15x_ByMeter_da_counts)
+
+# What do these abundances look like in the forest, patch, and edge?
+forest_sum <- rowSums(ASVtab_52_15x_ByMeter_da_counts[,6:10]) #gets sum across each row of ONLY the forest samples
+forest_meanAbund <- forest_sum/5 #average number in each meter in the forest
+patch_sum <- rowSums(ASVtab_52_15x_ByMeter_da_counts[,1:4])
+patch_meanAbund <- patch_sum/4
+edge_meanAbund <- ASVtab_52_15x_ByMeter_da_counts[,5] # 50m, and it is still basically a mean since this is across multiple edge samples (up to four
+# I don't remember which samples were dropped here)
+
+# Combine it all together-- NON RELATIVE ABUNDANCE-- this is kinda like all the info!
+ASVtab_52_15x_ByMeter_all <- cbind(ASVtab_52_15x_ByMeter_da_counts, forest_meanAbund, patch_meanAbund, edge_meanAbund)
+head(ASVtab_52_15x_ByMeter_all)
+dim(ASVtab_52_15x_ByMeter_all)
+#View(ASVtab_52_15x_ByMeter_all)
+
+# Convert the counts to relative abundance
+rowmax <- rep(NA, nrow(ASVtab_52_15x_ByMeter_da_counts)) #make an empty vector to store rowmaxes
+for (i in 1:nrow(ASVtab_52_15x_ByMeter_da_counts)) {
+  rowmax[i] <- max(ASVtab_52_15x_ByMeter_da_counts[i, 1:10]) #get maximum abundance of each ASV
+  relAbund_52_15x_ByMeter <- ASVtab_52_15x_ByMeter_da_counts[,1:10]/rowmax
+}
+relAbund_52_15x_ByMeter
+#View(relAbund_52_10x_ByMeter) # I manually calculated a few of these and it looks good!
+
+# Plot with Base R
+relAbund_52_15x_ByMeter <- t(relAbund_52_15x_ByMeter) #now samples are x and ASVs are y
+# quartz()
+relAbund_52_15x_ByMeter_plot <- matplot(rownames(relAbund_52_15x_ByMeter), relAbund_52_15x_ByMeter, type = "l", xlab= "Meter",
+                                        ylab= "Relative Abundance", main= "Changes in ASV abundance across EU 52- Meters Combined")
+xtick<-seq(0, 100, by=10) 
+axis(side=1, at=xtick, labels = TRUE) #change x axis
+
+# Plot with ggplot and color by taxonomic group
+relAbund_52_15x_ByMeter_gg <- t(relAbund_52_15x_ByMeter) #switch it around again (now is ASVs as rows and meters as columns )
+ASVindex <- rownames(relAbund_52_15x_ByMeter_gg) #get all of these ASV names 
+taxTable52_15_ByMeter <- taxtable_outta_ps(EU_52_15_ByMeter.ps) #pull out tax table
+taxTable52_15x_ByMeter <- taxTable52_15_ByMeter[ASVindex,1:6] #get taxonomic information
+dim(taxTable52_15x_ByMeter) #35 ASVs , six taxonomic types (through genus)
+relAbund_52_15x_ByMeter_gg <- t(merge(relAbund_52_15x_ByMeter_gg, taxTable52_15x_ByMeter, by=0)) #by=0 makes it combine by rownames
+# View(relAbund_52_15x_ByMeter_gg) #rows are now meters and taxonomic info, columns are ASVs
+relAbund_52_15x_ByMeter_gg <- header.true(relAbund_52_15x_ByMeter_gg) #lose ASV names but that's okay
+colnames(relAbund_52_15x_ByMeter_gg) <- rownames(taxTable52_15x_ByMeter) #add ASV names back in
+relAbund_52_15x_ByMeter_gg <- as.data.frame(relAbund_52_15x_ByMeter_gg) #makes values numeric again!
+tax <- t(relAbund_52_15x_ByMeter_gg[11:16,]) #grab and save taxonomic information and invert it so I can make it into columns
+relAbund_52_15x_ByMeter_gg <- relAbund_52_15x_ByMeter_gg[1:10,] #remove last rows that are taxonomic info
+# help with ggplot from here: https://community.rstudio.com/t/how-plot-all-values-inside-a-data-frame-into-a-graph-using-ggplot-function/75021
+relAbund_52_15x_ByMeter_gg <- rownames_to_column(relAbund_52_15x_ByMeter_gg, var="Meter")
+relAbund_52_15x_ByMeter_gg <- relAbund_52_15x_ByMeter_gg %>% pivot_longer(cols= ASV_7:ASV_2422,
+                                                                          names_to= "ASV_name", values_to = "ASV_Rel_Abundance")
+head(relAbund_52_15x_ByMeter_gg) #now each ASV in each sample has a value
+tail(relAbund_52_15x_ByMeter_gg) #no more taxonomic info on the end
+relAbund_52_15x_ByMeter_gg <- cbind.data.frame(relAbund_52_15x_ByMeter_gg, tax) #tax is now columns
+# Make it so the meters are plotted in the correct order
+relAbund_52_15x_ByMeter_gg$Meter <- factor(relAbund_52_15x_ByMeter_gg$Meter, levels = c("10", "20", "30", "40", "50","60", "70", "80", "90", "100"))
+# Finally, plot this!
+
+quartz()
+ggplot(relAbund_52_15x_ByMeter_gg, aes(Meter, ASV_Rel_Abundance, color = Phylum, group = ASV_name)) + 
+  geom_line() + theme(axis.text.y = element_blank()) + ggtitle("Changes in ASV abundance across EU 52 (combined by meter)")
 
 
 ######### BY TRANSECT #########
@@ -337,7 +456,7 @@ ASVtab_52_10x_T # top transect
 ASVtab_52_10x_T <- t(ASVtab_52_10x_T[,1:8])
 rownames(ASVtab_52_10x_T) #mssing 70m
 ASVtab_52_10x_T <- ASVtab_52_10x_T[c("10", "20", "30", "40", "60", "80", "90", "100"),] #rorder to make 10m - 100m
-View(ASVtab_52_10x_T)
+# View(ASVtab_52_10x_T)
 
 ASVtab_52_10x_B <- ASVtab_52_10x_da_50[,c(EU_52_Bindex,35:38)] 
 colnames(ASVtab_52_10x_B)[1:8] <- sample_data(EU_52_10timesNoEdge.ps)$Meter[EU_52_Bindex]
@@ -454,3 +573,4 @@ barplot(table(ASVwithCount_54S[,39]), main= "EU 54S", ylab="Number of ASVs", xla
 barplot(table(ASVwithCount_8[,40]), main= "EU 8", ylab="Number of ASVs", xlab= "Number of Samples Present in (out of 39)" )
 barplot(table(ASVwithCount_53S[,41]), main= "EU 53S", ylab="Number of ASVs", xlab= "Number of Samples Present in (out of 40)" )
 barplot(table(ASVwithCount_10[,40]), main= "EU 10", ylab="Number of ASVs", xlab= "Number of Samples Present in (out of 39)")
+
