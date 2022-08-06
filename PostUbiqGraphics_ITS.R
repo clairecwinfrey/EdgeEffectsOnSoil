332e8d608f9b6fbeddb07eee599b397b13bcfde4
Claire_C_Winfrey <clairecwinfrey@gmail.com>
2022-08-06 16:19:32 -0600

    edited habitat type ordination plot

--- PostUbiqGraphics_ITS.R
+++ PostUbiqGraphics_ITS.R
@@ -28,10 +28,18 @@ load("RobjectsSaved/ITS_postUbiquity.ps") #load phyloseq object that was made af
 set.seed(19)
 ord_ITS <- ordinate(ITS_postUbiquity.ps, method = "NMDS", distance = "bray", trymax = 100) 
 
-# Ordination based on "habitat"/ plant community type
+# Ordination based on "habitat"/ plant community type (added to ESA presentation Aug 8)
 HabitatNMDS_ITS_postUbiq <- phyloseq::plot_ordination(ITS_postUbiquity.ps, ord_ITS, type= "samples", color= "Habitat")
-HabitatNMDS_ITS_postUbiq <- HabitatNMDS_ITS_postUbiq + geom_polygon(aes(fill=Habitat)) + geom_point(size=3) + ggtitle("NMDS based on Bray-Curtis Dissimilarities")
+HabitatNMDS_ITS_postUbiq <- HabitatNMDS_ITS_postUbiq +
+   scale_color_manual(values=c("purple", "darkgreen", "goldenrod")) + #change color of points
+   geom_point(size=3) + ggtitle("(Fungal) NMDS based on Bray-Curtis Dissimilarities") +
+  theme_bw() +
+  guides(color = guide_legend(override.aes = list(size = 7))) + #change size of legend and title
+quartz()
 HabitatNMDS_ITS_postUbiq #cool, you can see that the forest and the patch separate out, with edge somewhat in between!
+
+# Save the plot made above (saved Aug. 6, 2022) so that we can make a two paneled figure with 16S stuff
+save(HabitatNMDS_ITS_postUbiq, file="RobjectsSaved/diffAbund_ITS_stackedBarplotPhyla_plot")
 
 # Ordination based on EU (just to show that they are different!)
 EU_NMDS_ITS_postUbiq <- phyloseq::plot_ordination(ITS_postUbiquity.ps, ord_ITS, type= "samples", color= "EU")
