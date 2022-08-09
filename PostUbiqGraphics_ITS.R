a3a7d097d43ffc66fb9212fbef362c327c07c803
Claire_C_Winfrey <clairecwinfrey@gmail.com>
2022-08-09 11:54:09 -0600

    Performed PERMANOVA to see if samples (post Ubiquity) differ by habitat type. Unsurprisingly, they do!

--- PostUbiqGraphics_ITS.R
+++ PostUbiqGraphics_ITS.R
@@ -14,12 +14,30 @@ library("phyloseq")
 library("tidyverse")    
 library("vegan")
 library("gridExtra")    # allows you to make multiple plots on the same page with ggplot
+library("utilities")
 
 # Load data
 load("RobjectsSaved/ITS_postUbiquity.ps") #load phyloseq object that was made after 1) rarefying,
 # the 2) keeping only ASVs that occurred at least 50 times across the (rarefied), 
 # then 3) filtering out ASVs with a ubiquity of <40
 # R OBJECT MADE IN: EdgeEffectsAllSitesFUNGI.R
+
+# FUNCTIONS DEFINED IN THIS SCRIPT (but often used first in other scripts):
+# 1. taxtable_outta_ps takes the phyloseq ASV table and converts it to a dataframe
+taxtable_outta_ps <- function(physeq){ #input is a phyloseq object
+  taxTable <- tax_table(physeq)
+  return(as.data.frame(taxTable))
+}
+
+# 2. ASVs_outta_ps gets the ASV table out of phyloseq 
+# from: https://jacobrprice.github.io/2017/08/26/phyloseq-to-vegan-and-back.html
+ASVs_outta_ps <- function(physeq){ #input is a phyloseq object
+  ASVTable <- otu_table(physeq)
+  if(taxa_are_rows(ASVTable)) {
+    ASVTable <- t(ASVTable)
+  }
+  return(as.data.frame(ASVTable))
+}
 
 #################################
 # ORDINATIONS
@@ -38,7 +56,7 @@ HabitatNMDS_ITS_postUbiq <- HabitatNMDS_ITS_postUbiq +
 quartz()
 HabitatNMDS_ITS_postUbiq #cool, you can see that the forest and the patch separate out, with edge somewhat in between!
 
-# Save the plot made above (saved Aug. 6, 2022) so that we can make a two paneled figure with 16S stuff
+# Save the plot made above (saved Aug. 6, 2022) so that we can make a two paneled figure with ITS stuff
 save(HabitatNMDS_ITS_postUbiq, file="RobjectsSaved/diffAbund_ITS_stackedBarplotPhyla_plot")
 
 # Ordination based on EU (just to show that they are different!)
@@ -46,5 +64,32 @@ EU_NMDS_ITS_postUbiq <- phyloseq::plot_ordination(ITS_postUbiquity.ps, ord_ITS, 
 EU_NMDS_ITS_postUbiq <- EU_NMDS_ITS_postUbiq + geom_polygon(aes(fill=EU)) + geom_point(size=3) + ggtitle("NMDS based on Bray-Curtis Dissimilarities")
 EU_NMDS_ITS_postUbiq #cool, you can see that the forest and the patch separate out, with edge somewhat in between!
 
-quartz()
+#quartz()
 grid.arrange(EU_NMDS_ITS_postUbiq, HabitatNMDS_ITS_postUbiq, ncol=2)
+
+#########################################################################
+# PERMANOVA TO TEST FOR THE EFFECT OF HABITAT TYPE ON THE DISTRIBUTIONS
+#########################################################################
+
+# First, need to get data out of phyloseq so that it can be processed in vegan
+postUbiq_ITSASVs <- ASVs_outta_ps(ITS_postUbiquity.ps) #get ASV table
+str(postUbiq_ITSASVs) #samples are rows, taxa are columns as they should be for vegan
+# Getting rid of the phyloseq attribute for metadata is near impossible, so I have to hack it a bit
+# by grabbing each column that I need (as vectors or strings), then combining these
+sampNames <- rownames(sample_data(ITS_postUbiquity.ps))
+rownames(postUbiq_ITSASVs) == sampNames #since these match, we are good to go!
+habitat <- sample_data(ITS_postUbiquity.ps)$Habitat
+sampID <- sample_data(ITS_postUbiquity.ps)$Sample.ID
+metaDatForVeg <- cbind(sampID, habitat)
+rownames(metaDatForVeg) <- sampNames
+metaDatForVeg <- as.data.frame(metaDatForVeg)
+
+# Next get Bray-Curtis dissimilarities
+postUbiq_ITSBC <- vegdist(postUbiq_ITSASVs, method= "bray")
+set.seed(19) #set seed so that results are reproducible!
+# Run the PERMANOVA
+postUbiq_ITSPermanova <- adonis(postUbiq_ITSBC ~ habitat, data=metaDatForVeg, permutations= 99999)
+# p < 0.001
+
+# Save PERMANOVA results
+# save(postUbiq_ITSPermanova, file= "RobjectsSaved/postUbiq_ITSPermanova") #last saved Aug 9, 2022
