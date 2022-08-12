8781c784cbd09735ad1d6620c686241f14061e0b
Claire_C_Winfrey <clairecwinfrey@gmail.com>
2022-08-11 18:57:52 -0600

    first commit of script preparing for and taking a look at FUNGuild results

---
+++ FUNGuildExploration.R
@@ -0,0 +1,84 @@
+# FUNGuildExploration.R
+# Aug 11, 2022
+
+###################################################################################################
+# This script formats data for and then explores FUNGuild Output
+###################################################################################################
+
+# Set working directory
+setwd("~/Desktop/CU_Research/SoilEdgeEffectsResearch")
+
+# Read in libraries
+library("phyloseq")       
+library("tidyverse")       
+library("vegan")
+library("ggplot2")
+
+# Load data
+load("RobjectsSaved/ITS_postUbiquity.ps") #load phyloseq object that was made after rarefying,
+# and keeping only ASVs that occurred at least 50 times across the (rarefied) 
+# dataset (from ITS_UbiquityMedianSetup.R).
+
+# FUNCTIONS DEFINED IN THIS SCRIPT:
+# 1. taxtable_outta_ps takes the phyloseq ASV table and converts it to a dataframe
+taxtable_outta_ps <- function(physeq){ #input is a phyloseq object
+  taxTable <- tax_table(physeq)
+  return(as.data.frame(taxTable))
+}
+
+# 2. ASVs_outta_ps gets the ASV table out of phyloseq 
+# from: https://jacobrprice.github.io/2017/08/26/phyloseq-to-vegan-and-back.html
+ASVs_outta_ps <- function(physeq){ #input is a phyloseq object
+  ASVTable <- otu_table(physeq)
+  if(taxa_are_rows(ASVTable)) {
+    ASVTable <- t(ASVTable)
+  }
+  return(as.data.frame(ASVTable))
+}
+
+# 3. # taxtable_outta_ps takes the phyloseq ASV table and converts it to a dataframe
+taxtable_outta_ps <- function(physeq){ #input is a phyloseq object
+  taxTable <- tax_table(physeq)
+  return(as.data.frame(taxTable))
+}
+
+###############################################################################
+#         FORMATTING ITS_postUbiquity.ps FOR FUNGuild 
+###############################################################################
+# format is tsv, where first row should be OTU_IOTU_ID(tab)sample1(tab)sample2(tab)sample3(tab)taxonomy(return)
+# taxonomic levels should be separated by semicolons
+
+postUbiqITS_ASVtab <- t(ASVs_outta_ps(ITS_postUbiquity.ps)) #get ASV table out of phyloseq, ASVs are rows
+postUbiqITS_TaxTab <- taxtable_outta_ps(ITS_postUbiquity.ps) #get tax table out of phyloseq; ASVs are also rows!
+rownames(postUbiqITS_TaxTab) == rownames(postUbiqITS_ASVtab)
+FUNGuildTable1 <- merge(postUbiqITS_ASVtab, postUbiqITS_TaxTab, by= "row.names", all=TRUE)
+colnames(FUNGuildTable1)[1] <- "OTU_ID"
+# Collapse taxonomy into one column 
+FUNGuildTable1 <- tidyr::unite(FUNGuildTable1, sep=";",col= "taxonomy", Kingdom:Species)
+# View(FUNGuildTable1)
+# Remove all of the pesky stuff before taxonomy
+FUNGuildTable1$taxonomy <-gsub("k__","",as.character(FUNGuildTable1$taxonomy))
+FUNGuildTable1$taxonomy <-gsub("p__","",as.character(FUNGuildTable1$taxonomy))
+FUNGuildTable1$taxonomy <-gsub("c__","",as.character(FUNGuildTable1$taxonomy))
+FUNGuildTable1$taxonomy <-gsub("o__","",as.character(FUNGuildTable1$taxonomy))
+FUNGuildTable1$taxonomy <-gsub("f__","",as.character(FUNGuildTable1$taxonomy))
+FUNGuildTable1$taxonomy <-gsub("g__","",as.character(FUNGuildTable1$taxonomy))
+FUNGuildTable1$taxonomy <-gsub("s__","",as.character(FUNGuildTable1$taxonomy))
+# colnames(FUNGuildTable1)
+
+# Written to file Aug. 10, 2022
+write.table(FUNGuildTable1, file = "FUNGuildTable.txt", sep = "\t",
+            row.names = TRUE, col.names = TRUE)
+# ** Finally, remove any quotation marks that may appear in new file in a text editor (I used Atom) and make
+# sure that column names and data line up.
+
+# SEE THE FILE FUNGuildStepByStep for how I ran FUNGuild
+
+###############################################################################
+#         Exploring FUNGuild Results
+###############################################################################
+
+# load file:
+fungResults <- read.delim(file= "~/Desktop/CU_Research/SoilEdgeEffectsResearch/FUNGuildTable.guilds_matched.txt", header = T)
+# View(fungResults) #has X in front of all the sample names, but this should be okay!
+# Out of the
