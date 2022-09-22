4a92b1672ff0fb9b366db953ba9412235b61c199
Claire_C_Winfrey <clairecwinfrey@gmail.com>
2022-09-22 13:03:08 -0600

    finished plots for average differentially abundant ASV means (per EU) for both prokaryotes and fungi!

--- DissimAcrossTransectDiffAbundOnly.R
+++ DissimAcrossTransectDiffAbundOnly.R
@@ -304,7 +304,7 @@ prok_EULists <- prokaryotesASVmeans_AllEUs %>%  #make multiple datasets based on
   dplyr::group_split() 
 names(prok_EULists) <- sort(unique(prokaryotesASVmeans_AllEUs$EU))
 
-# Use a for loop to make ASV_names row names 
+# Use a for loop to make ASV_names row names (have to do this step after splitting by EUs so that there are no duplicated row names)
 for (i in 1:length(prok_EULists)) {
   prok_EULists[[i]] <- prok_EULists[[i]] %>% 
     column_to_rownames("ASV_name")  #make sampNames column in the rownames
@@ -367,8 +367,6 @@ for (i in 1:length(prok_EUsLists)){ #there are 6 dataframes within this, corresp
 }
 names(proks_meanASVs_BCs) <- names(prok_EUsLists) #make names the same as EULists (works because for loop above runs through EU Lists)
 
-######## THIS IS WHERE I ENDED ON SEPTEMBER 22, 2022!! ####
-
 # Get 10 m and 100 meter Bray-Curtis distances
 ## Comparisons with both 10m and 100m
 prok_meanComps <- vector("list", 6) #pre-allocate space for all comps with 10 m for each EU
@@ -382,27 +380,120 @@ for (i in 1:length(prok_meanComps)){
     #Make third column 100m comps
     prok_meanComps[[i]][j,3] <- proks_meanASVs_BCs[[i]][j,10]
     #Make fourth column in comps10 EU
-    prok_meanComps[[i]][j,4] <- prok_EUsLists[[i]]$EU[j]
+    prok_meanComps[[i]][j,4] <- names(prok_EUsLists)[[i]]
   }
   colnames(prok_meanComps[[i]]) <- c("meter", "comp10m", "comp100m", "EU")
 }
-names(comps) <- names(BCs)
+names(prok_meanComps) <- names(proks_meanASVs_BCs)
 
 # Use a for loop to arrange data in a way compatible with ggplot2
-compsForPlot <- as.data.frame(matrix(data=NA, nrow= 120, ncol=4))
-for (i in 1:length(comps)) {
-  comps[[i]] <- as.data.frame(comps[[i]]) %>% 
+prok_compsForPlot <- as.data.frame(matrix(data=NA, nrow= 120, ncol=4))
+for (i in 1:length(prok_meanComps)) {
+  prok_meanComps[[i]] <- as.data.frame(prok_meanComps[[i]]) %>% 
     pivot_longer(comp10m:comp100m,
                  names_to= "compType",
                  values_to = "BCdists")
 }
 
 # Combine all of the data!
-BCcompsPlot <- bind_rows(comps$EU_10, comps$EU_52, comps$EU_53N, comps$EU_53S, comps$EU_54S, comps$EU_8)
-BCcompsPlot$meter <- as.numeric(BCcompsPlot$meter) 
-BCcompsPlot$BCdists <- as.numeric(BCcompsPlot$BCdists)
+prok_BCcompsPlot <- bind_rows(prok_meanComps$EU_10, prok_meanComps$EU_52, prok_meanComps$EU_53N, 
+                              prok_meanComps$EU_53S, prok_meanComps$EU_54S, prok_meanComps$EU_8)
+prok_BCcompsPlot$meter <- as.numeric(prok_BCcompsPlot$meter) 
+prok_BCcompsPlot$BCdists <- as.numeric(prok_BCcompsPlot$BCdists)
+
+### *NEED TO MAKE SURE THAT IT IS WORKING AS EXPECTED since the code was co-opted from DissimilaritiesAcrosTransectsMedians.R
+
+#. PLOTTING USING DATAFRAME CREATED ABOVE-- BUT MAKE COMPARISONS ONLY WITH 100M INTO FOREST
+
+# EU 10
+# comparison with forest 100m only
+# Matrix comparison lines ONLY (added to ESA 2022 presentation)
+# get subset of data for only forest comparison
+EU_10_prok_BCsforestOnly <- prok_BCcompsPlot[1:20,] %>% 
+  filter(compType == "comp100m")
+
+ggEU_10_prok_forestOnly <- ggplot(data=EU_10_prok_BCsforestOnly, aes(x=meter, y=BCdists)) + 
+  geom_line(size=2.7, color= "darkgreen") +
+  theme_bw() + ylim(0.1, 1.00) +
+  scale_x_continuous("Transect Meter", breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
+  labs(title= "EU 10 (prokaryotes)", y = "Bray-Curtis dissimilarity", size= 14) + 
+  geom_vline(xintercept = 50, linetype= "dashed", color= "darkgrey", size=1.5) + 
+  theme(text = element_text(size=18)) +
+  theme(legend.position = "none")
+# quartz()
+ggEU_10_prok_forestOnly 
+
+# EU 52
+EU_52_prok_BCsforestOnly <- prok_BCcompsPlot[21:40,] %>% 
+  filter(compType == "comp100m")
+
+ggEU_52_prok_forestOnly <- ggplot(data=EU_52_prok_BCsforestOnly, aes(x=meter, y=BCdists)) + 
+  geom_line(size=2.7, color= "darkgreen") +
+  theme_bw() + ylim(0.1, 1.00) +
+  scale_x_continuous("Transect Meter", breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
+  labs(title= "EU 52 (prokaryotes)", y = "Bray-Curtis dissimilarity", size= 14) + 
+  geom_vline(xintercept = 50, linetype= "dashed", color= "darkgrey", size=1.5) + 
+  theme(text = element_text(size=18)) +
+  theme(legend.position = "none")
+# quartz()
+ggEU_52_prok_forestOnly
+
+# EU 53N
+EU_53N_prok_BCsforestOnly <- prok_BCcompsPlot[41:60,] %>% 
+  filter(compType == "comp100m")
+
+ggEU_53N_prok_forestOnly <- ggplot(data=EU_53N_prok_BCsforestOnly, aes(x=meter, y=BCdists)) + 
+  geom_line(size=2.7, color= "darkgreen") +
+  theme_bw() + ylim(0.1, 1.00) +
+  scale_x_continuous("Transect Meter", breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
+  labs(title= "EU 53N (prokaryotes)", y = "Bray-Curtis dissimilarity", size= 14) + 
+  geom_vline(xintercept = 50, linetype= "dashed", color= "darkgrey", size=1.5) + 
+  theme(text = element_text(size=18)) +
+  theme(legend.position = "none")
+
+# EU 53S
+EU_53S_prok_BCsforestOnly <- prok_BCcompsPlot[61:80,] %>% 
+  filter(compType == "comp100m")
+
+ggEU_53S_prok_forestOnly <- ggplot(data=EU_53S_prok_BCsforestOnly, aes(x=meter, y=BCdists)) + 
+  geom_line(size=2.7, color= "darkgreen") +
+  theme_bw() + ylim(0.1, 1.00) +
+  scale_x_continuous("Transect Meter", breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
+  labs(title= "EU 53S (prokaryotes)", y = "Bray-Curtis dissimilarity", size= 14) + 
+  geom_vline(xintercept = 50, linetype= "dashed", color= "darkgrey", size=1.5) + 
+  theme(text = element_text(size=18)) +
+  theme(legend.position = "none")
 
 
+# EU 54S
+EU_54S_prok_BCsforestOnly <- prok_BCcompsPlot[81:100,] %>% 
+  filter(compType == "comp100m")
+
+ggEU_54S_prok_forestOnly <- ggplot(data=EU_54S_prok_BCsforestOnly, aes(x=meter, y=BCdists)) + 
+  geom_line(size=2.7, color= "darkgreen") +
+  theme_bw() + ylim(0.1, 1.00) +
+  scale_x_continuous("Transect Meter", breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
+  labs(title= "EU 54S (prokaryotes)", y = "Bray-Curtis dissimilarity", size= 14) + 
+  geom_vline(xintercept = 50, linetype= "dashed", color= "darkgrey", size=1.5) + 
+  theme(text = element_text(size=18)) +
+  theme(legend.position = "none")
+
+# EU 8
+EU_8_prok_BCsforestOnly <- prok_BCcompsPlot[101:120,] %>% 
+  filter(compType == "comp100m")
+
+ggEU_8_prok_forestOnly <- ggplot(data=EU_8_prok_BCsforestOnly, aes(x=meter, y=BCdists)) + 
+  geom_line(size=2.7, color= "darkgreen") +
+  theme_bw() + ylim(0.1, 1.00) +
+  scale_x_continuous("Transect Meter", breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
+  labs(title= "EU 8 (prokaryotes)", y = "Bray-Curtis dissimilarity", size= 14) + 
+  geom_vline(xintercept = 50, linetype= "dashed", color= "darkgrey", size=1.5) + 
+  theme(text = element_text(size=18)) +
+  theme(legend.position = "none")
+
+##### PLOT ALL TOGETHER #####
+quartz()
+grid.arrange(ggEU_10_prok_forestOnly, ggEU_52_prok_forestOnly, ggEU_53N_prok_forestOnly, ggEU_53S_prok_forestOnly, ggEU_54S_prok_forestOnly, ggEU_8_prok_forestOnly, ncol=3)
 
 ###############################################
 # FUNGI
@@ -518,3 +609,209 @@ grid.arrange(fung_ggEU_52, fung_ggEU_53N, fung_ggEU_54S, fung_ggEU_8, fung_ggEU_
 
 # 2. Getting dissimilarity values where MEAN is that used to calculate z-scores
 # (i.e. each differentially abundant ASV at each meter)
+
+fungiASVmeans_AllEUs #made in fungiEdgeEffectsZscores.R
+colnames(fungiASVmeans_AllEUs)
+
+# 1. CALCULATE BRAY-CURTIS DISSIMILARITIES ALONG TRANSECTS WITHIN EACH EU
+
+# The lines below get the Bray-Curtis dissimilarities along the transect, comparing each point along
+# the transect to 10 meters and to 100 meters 
+
+# The piped code below creates a list of 6 tibbles, corresponding to each EU
+fung_EULists <- fungiASVmeans_AllEUs %>%  #make multiple datasets based on EU
+  dplyr::group_by(EU) %>%  #group by EU
+  dplyr::group_split() 
+names(fung_EULists) <- sort(unique(fungiASVmeans_AllEUs$EU))
+
+# Use a for loop to make ASV_names row names (have to do this step after splitting by EUs so that there are no duplicated row names)
+for (i in 1:length(fung_EULists)) {
+  fung_EULists[[i]] <- fung_EULists[[i]] %>% 
+    column_to_rownames("ASV_name")  #make sampNames column in the rownames
+}
+
+# For vegdist, rows need to be samples and columns should be species.
+# 1) pull out only mean ASV abundances, 2) make ASV names the rownames,
+# and 3) flip it all!
+fung_EU_10_smaller <- as.data.frame(fung_EULists$EU_10[,1:10])
+rownames(fung_EU_10_smaller) <- rownames(fung_EULists$EU_10)
+fung_EU_10_smaller_t <- as.data.frame(t(fung_EU_10_smaller))
+fung_EU_10_smaller_t$Meter <- c("10", "20", "30", "40", "50", "60",
+                                "70", "80", "90", "100")
+
+fung_EU_52_smaller <- as.data.frame(fung_EULists$EU_52[,1:10])
+rownames(fung_EU_52_smaller) <- rownames(fung_EULists$EU_52)
+fung_EU_52_smaller_t <- as.data.frame(t(fung_EU_52_smaller))
+fung_EU_52_smaller_t$Meter <-  c("10", "20", "30", "40", "50", "60",
+                                 "70", "80", "90", "100")
+
+fung_EU_53N_smaller <- as.data.frame(fung_EULists$EU_53N[,1:10])
+rownames(fung_EU_53N_smaller) <- rownames(fung_EULists$EU_53N)
+fung_EU_53N_smaller_t <- as.data.frame(t(fung_EU_53N_smaller))
+fung_EU_53N_smaller_t$Meter <-  c("10", "20", "30", "40", "50", "60",
+                                  "70", "80", "90", "100")
+
+fung_EU_53S_smaller <- as.data.frame(fung_EULists$EU_53S[,1:10])
+rownames(fung_EU_53S_smaller) <- rownames(fung_EULists$EU_53S)
+fung_EU_53S_smaller_t <- as.data.frame(t(fung_EU_53S_smaller))
+fung_EU_53S_smaller_t$Meter <-  c("10", "20", "30", "40", "50", "60",
+                                  "70", "80", "90", "100")
+
+fung_EU_54S_smaller <- as.data.frame(fung_EULists$EU_54S[,1:10])
+rownames(fung_EU_54S_smaller) <- rownames(fung_EULists$EU_54S)
+fung_EU_54S_smaller_t <- as.data.frame(t(fung_EU_54S_smaller))
+fung_EU_54S_smaller_t$Meter <-  c("10", "20", "30", "40", "50", "60",
+                                  "70", "80", "90", "100")
+
+fung_EU_8_smaller <- as.data.frame(fung_EULists$EU_8[,1:10])
+rownames(fung_EU_8_smaller) <- rownames(fung_EULists$EU_8)
+fung_EU_8_smaller_t <- as.data.frame(t(fung_EU_8_smaller))
+fung_EU_8_smaller_t$Meter <-  c("10", "20", "30", "40", "50", "60",
+                                "70", "80", "90", "100")
+
+# Make all of these into a list
+fung_EUsLists <- list(fung_EU_10_smaller_t, fung_EU_52_smaller_t, fung_EU_53N_smaller_t, fung_EU_53S_smaller_t, 
+                      fung_EU_54S_smaller_t, fung_EU_8_smaller_t)
+length(fung_EUsLists) #6, as expected
+names(fung_EUsLists) <- c("EU_10", "EU_52", "EU_53N", "EU_53S", "EU_54S", "EU_8")
+
+# Calculate Bray-Curtis dissimilarities within EUs (as created above)
+fungs_meanASVs_BCs <- vector("list", 6) #pre-allocate vector to store Bray-Curtis dissimilarities
+for (i in 1:length(fung_EUsLists)){ #there are 6 dataframes within this, corresponding to each EU. 
+  # they are in numerical order, starting with EU 10
+  # Get Bray-Curtis dissimilarities for a subset (not last three rows)
+  fungs_meanASVs_BCs[[i]] <- as.matrix(vegdist(fung_EUsLists[[i]][,1:ncol(fung_EUsLists[[i]])-1], method="bray")) #all but meter column
+  diag(fungs_meanASVs_BCs[[i]]) <- NA
+  rownames(fungs_meanASVs_BCs[[i]]) <- rownames(fung_EUsLists[[i]]) #keep rownames (for whatever reason it won't work automatically)
+  colnames(fungs_meanASVs_BCs[[i]]) <- rownames(fung_EUsLists[[i]]) #keep rownames (for whatever reason it won't work automatically)
+}
+names(fungs_meanASVs_BCs) <- names(fung_EUsLists) #make names the same as EULists (works because for loop above runs through EU Lists)
+
+# Get 10 m and 100 meter Bray-Curtis distances
+## Comparisons with both 10m and 100m
+fung_meanComps <- vector("list", 6) #pre-allocate space for all comps with 10 m for each EU
+for (i in 1:length(fung_meanComps)){
+  #Below, pre-allocate matrix as each element in the list
+  fung_meanComps[[i]] <- matrix(data=NA, nrow= nrow(fungs_meanASVs_BCs[[i]]), ncol=4)
+  for (j in 1:nrow(fung_meanComps[[1]])) {
+    #Make first column in comps10 "Meter" from EULists
+    fung_meanComps[[i]][j,1] <- fung_EUsLists[[i]]$Meter[j]
+    fung_meanComps[[i]][j,2] <- fungs_meanASVs_BCs[[i]][j,1] #so this is each row, adding in col 1 comparisons (will have some NAs for comparison with 10m)
+    #Make third column 100m comps
+    fung_meanComps[[i]][j,3] <- fungs_meanASVs_BCs[[i]][j,10]
+    #Make fourth column in comps10 EU
+    fung_meanComps[[i]][j,4] <- names(fung_EUsLists)[[i]]
+  }
+  colnames(fung_meanComps[[i]]) <- c("meter", "comp10m", "comp100m", "EU")
+}
+names(fung_meanComps) <- names(fungs_meanASVs_BCs)
+
+# Use a for loop to arrange data in a way compatible with ggplot2
+fung_compsForPlot <- as.data.frame(matrix(data=NA, nrow= 120, ncol=4))
+for (i in 1:length(fung_meanComps)) {
+  fung_meanComps[[i]] <- as.data.frame(fung_meanComps[[i]]) %>% 
+    pivot_longer(comp10m:comp100m,
+                 names_to= "compType",
+                 values_to = "BCdists")
+}
+
+# Combine all of the data!
+fung_BCcompsPlot <- bind_rows(fung_meanComps$EU_10, fung_meanComps$EU_52, fung_meanComps$EU_53N, 
+                              fung_meanComps$EU_53S, fung_meanComps$EU_54S, fung_meanComps$EU_8)
+fung_BCcompsPlot$meter <- as.numeric(fung_BCcompsPlot$meter) 
+fung_BCcompsPlot$BCdists <- as.numeric(fung_BCcompsPlot$BCdists)
+
+### *NEED TO MAKE SURE THAT IT IS WORKING AS EXPECTED since the code was co-opted from DissimilaritiesAcrosTransectsMedians.R
+
+#. PLOTTING USING DATAFRAME CREATED ABOVE-- BUT MAKE COMPARISONS ONLY WITH 100M INTO FOREST
+
+# EU 10
+# comparison with forest 100m only
+# Matrix comparison lines ONLY (added to ESA 2022 presentation)
+# get subset of data for only forest comparison
+EU_10_fung_BCsforestOnly <- fung_BCcompsPlot[1:20,] %>% 
+  filter(compType == "comp100m")
+
+ggEU_10_fung_forestOnly <- ggplot(data=EU_10_fung_BCsforestOnly, aes(x=meter, y=BCdists)) + 
+  geom_line(size=2.7, color= "darkgreen") +
+  theme_bw() + ylim(0.1, 1.00) +
+  scale_x_continuous("Transect Meter", breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
+  labs(title= "EU 10 (fungi)", y = "Bray-Curtis dissimilarity", size= 14) + 
+  geom_vline(xintercept = 50, linetype= "dashed", color= "darkgrey", size=1.5) + 
+  theme(text = element_text(size=18)) +
+  theme(legend.position = "none")
+# quartz()
+ggEU_10_fung_forestOnly 
+
+# EU 52
+EU_52_fung_BCsforestOnly <- fung_BCcompsPlot[21:40,] %>% 
+  filter(compType == "comp100m")
+
+ggEU_52_fung_forestOnly <- ggplot(data=EU_52_fung_BCsforestOnly, aes(x=meter, y=BCdists)) + 
+  geom_line(size=2.7, color= "darkgreen") +
+  theme_bw() + ylim(0.1, 1.00) +
+  scale_x_continuous("Transect Meter", breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
+  labs(title= "EU 52 (fungi)", y = "Bray-Curtis dissimilarity", size= 14) + 
+  geom_vline(xintercept = 50, linetype= "dashed", color= "darkgrey", size=1.5) + 
+  theme(text = element_text(size=18)) +
+  theme(legend.position = "none")
+# quartz()
+ggEU_52_fung_forestOnly
+
+# EU 53N
+EU_53N_fung_BCsforestOnly <- fung_BCcompsPlot[41:60,] %>% 
+  filter(compType == "comp100m")
+
+ggEU_53N_fung_forestOnly <- ggplot(data=EU_53N_fung_BCsforestOnly, aes(x=meter, y=BCdists)) + 
+  geom_line(size=2.7, color= "darkgreen") +
+  theme_bw() + ylim(0.1, 1.00) +
+  scale_x_continuous("Transect Meter", breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
+  labs(title= "EU 53N (fungi)", y = "Bray-Curtis dissimilarity", size= 14) + 
+  geom_vline(xintercept = 50, linetype= "dashed", color= "darkgrey", size=1.5) + 
+  theme(text = element_text(size=18)) +
+  theme(legend.position = "none")
+
+# EU 53S
+EU_53S_fung_BCsforestOnly <- fung_BCcompsPlot[61:80,] %>% 
+  filter(compType == "comp100m")
+
+ggEU_53S_fung_forestOnly <- ggplot(data=EU_53S_fung_BCsforestOnly, aes(x=meter, y=BCdists)) + 
+  geom_line(size=2.7, color= "darkgreen") +
+  theme_bw() + ylim(0.1, 1.00) +
+  scale_x_continuous("Transect Meter", breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
+  labs(title= "EU 53S (fungi)", y = "Bray-Curtis dissimilarity", size= 14) + 
+  geom_vline(xintercept = 50, linetype= "dashed", color= "darkgrey", size=1.5) + 
+  theme(text = element_text(size=18)) +
+  theme(legend.position = "none")
+
+
+# EU 54S
+EU_54S_fung_BCsforestOnly <- fung_BCcompsPlot[81:100,] %>% 
+  filter(compType == "comp100m")
+
+ggEU_54S_fung_forestOnly <- ggplot(data=EU_54S_fung_BCsforestOnly, aes(x=meter, y=BCdists)) + 
+  geom_line(size=2.7, color= "darkgreen") +
+  theme_bw() + ylim(0.1, 1.00) +
+  scale_x_continuous("Transect Meter", breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
+  labs(title= "EU 54S (fungi)", y = "Bray-Curtis dissimilarity", size= 14) + 
+  geom_vline(xintercept = 50, linetype= "dashed", color= "darkgrey", size=1.5) + 
+  theme(text = element_text(size=18)) +
+  theme(legend.position = "none")
+
+# EU 8
+EU_8_fung_BCsforestOnly <- fung_BCcompsPlot[101:120,] %>% 
+  filter(compType == "comp100m")
+
+ggEU_8_fung_forestOnly <- ggplot(data=EU_8_fung_BCsforestOnly, aes(x=meter, y=BCdists)) + 
+  geom_line(size=2.7, color= "darkgreen") +
+  theme_bw() + ylim(0.1, 1.00) +
+  scale_x_continuous("Transect Meter", breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
+  labs(title= "EU 8 (fungi)", y = "Bray-Curtis dissimilarity", size= 14) + 
+  geom_vline(xintercept = 50, linetype= "dashed", color= "darkgrey", size=1.5) + 
+  theme(text = element_text(size=18)) +
+  theme(legend.position = "none")
+
+##### PLOT ALL TOGETHER #####
+quartz()
+grid.arrange(ggEU_52_fung_forestOnly, ggEU_53N_fung_forestOnly, ggEU_54S_fung_forestOnly, 
+             ggEU_8_fung_forestOnly, ggEU_53S_fung_forestOnly, ggEU_10_fung_forestOnly, ncol=3)
