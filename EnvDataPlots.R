e288adef9faabc1d15a9f92ea1377f7b6197b9b7
Claire_C_Winfrey <clairecwinfrey@gmail.com>
2022-12-05 19:58:13 -0700

    fixed code so that it gets the mean pH for each point along the transect in each EU, instead of the median. Also removed cod making a plot that was no longer needed (and didn't work anyway)

--- EnvDataPlots.R
+++ EnvDataPlots.R
@@ -23,7 +23,15 @@ Mean_pH_func <- function(pHs_mat) { #enter a dataframe that has the pHs, as made
   return(avgpH)
 }
 
-# 2. metadata_outta_ps takes the phyloseq metadata table and converts it to a dataframe
+# 2. This function takes a vector of different pHs as input and returns a single mean pH (so it's
+# less complicated than Mean_pH_func)
+mean_pH_vecFunc <- function(pHs_vector){  
+  meanHydroniumConc <- mean(10^(pHs_vector*-1)) #gets mean hydronium concentrations of the pHs in vector
+  mean_pH <- -log10(meanHydroniumConc) #converts this back to pH by taking negative log of hydronium concentrations
+  return(mean_pH)
+} 
+
+# 3. metadata_outta_ps takes the phyloseq metadata table and converts it to a dataframe
 metadata_outta_ps <- function(physeq){ #input is a phyloseq object
   metaDat <- sample_data(physeq)
   return(as.data.frame(metaDat))
@@ -31,25 +39,35 @@ metadata_outta_ps <- function(physeq){ #input is a phyloseq object
 
 ############################################################
 
-load("Bioinformatics/medianEU.ps") #ps object with median abundances 
+load("RobjectsSaved/medianEU.ps") #ps object with median abundances 
 #load("Bioinformatics/trimmedJustsoils.ps") #this has the veg, canopy, pH that I need to average across transects
 allMetaDat <- read.csv(file="SRS_allMetadata_Dec8.csv") #created in DataCleaning_pH_Jul23
-View(allMetaDat)
+#View(allMetaDat)
 
 #### Get average pH for each EU at each point along the transect, by taking average of the 4 transects
 head(allMetaDat)
 class(allMetaDat)
-
-# Get average pH
-medianpH_EU <- allMetaDat %>% 
+# Get mean pH of all of the mean pHs for each EU at a given point along the transect
+meanpH_EU <- allMetaDat %>% 
   group_by(EU, Meter) %>% 
-  summarize(medpHEu = median(mean_pH))
-# View(medianpH_EU) #missing 53N_60 and 53N_100 because of an NA at 1/4 transects.
+  summarize(meanpHEu = mean_pH_vecFunc(mean_pH)) 
+# View(meanpH_EU) #missing 53N_60 and 53N_100 because of an NA at 1/4 transects.
 # Mssing T-60 and L_100 in 53N
 # So, I'll have to add it back in manually (row 26 and row 30)
-medianpH_EU <- medianpH_EU[-61,] #remove weird all NA final row
-medianpH_EU[26,3] <- median(allMetaDat$mean_pH[c(86,96,106)]) #median of values in (meanpHs of) 53N_B_60, 53N_L_60, and 53N_R_60
-medianpH_EU[30,3] <- median(allMetaDat$mean_pH[c(90,110,120)]) #median of values in (meanpHs of) 53N_B_100, 53N_R_100, and 53N_T_100
+meanpH_EU <- meanpH_EU[-61,] #remove weird all NA final row (these were controls so don't have pH)
+meanpH_EU[26,3] <- mean_pH_vecFunc(allMetaDat$mean_pH[c(86,96,106)]) #median of values in (meanpHs of) 53N_B_60, 53N_L_60, and 53N_R_60
+meanpH_EU[30,3] <- mean_pH_vecFunc(allMetaDat$mean_pH[c(90,110,120)]) #median of values in (meanpHs of) 53N_B_100, 53N_R_100, and 53N_T_100
+View(meanpH_EU)
+
+# These lines below show that getting pH is working as expected
+test <- intersect(which(allMetaDat$EU==10), which(allMetaDat$Meter == 100))
+EU10_100_all <- allMetaDat[test,]
+EU_10_100pHs <- c(EU10_100_all$pH_1, EU10_100_all$pH_2)
+
+EU_10_100_meanpH_step1 <-10^(EU_10_100pHs*-1)
+EU_10_100_meanpH_step2 <- mean(EU_10_100_meanpH_step1)
+EU_10_100_meanpH_final <- -log10(EU_10_100_meanpH_step2)
+mean_pH_vecFunc(EU_10_100pHs) ==   EU_10_100_meanpH_final #TRUE
 
 # Get average canopy cover
 avgCanopyEU <- allMetaDat %>% 
@@ -65,37 +83,14 @@ avgVegEU <- allMetaDat %>%
 avgVegEU <- avgVegEU[-61,] #remove weird last row of NAs
 
 # Before merge, check that EU and meter are in same order (should be because dplyr!)
-unique(medianpH_EU[,1:2] == avgCanopyEU[,1:2]) #all true
+unique(meanpH_EU[,1:2] == avgCanopyEU[,1:2]) #all true
 unique(avgCanopyEU[,1:2] == avgVegEU[,1:2]) #all true 
 
-pHCanVegMedEU <- cbind(medianpH_EU, avgCanopyEU[,3], avgVegEU[,3])
+pHCanVegMedEU <- cbind(meanpH_EU, avgCanopyEU[,3], avgVegEU[,3])
 View(pHCanVegMedEU)
 
 ######################
 # Plotting
-
-# A simple, base R plot for canopy cover and  vegetation cover (EU 8)
-quartz()
-plot(envEU8$Meter, 
-     envEU8$avgCanopyEu, type= "l",
-cex.lab=1.3, 
-xlab="Meter", 
-ylab="Percent Cover", 
-xaxp = c(10, 100, 9.9), 
-ylim=c(0,100), 
-cex=1.4,
-lwd=3)
-lines(envEU8$Meter, 
-      envEU8$avgVegCov, 
-      lwd=3,
-      lty="dashed")
-legend("topleft", 
-       legend= c("Canopy Cover", "Vegetation Cover"), 
-       lty= c("solid", "dashed"), 
-       bty= "o", 
-       lwd =2, 
-       cex=.8, 
-       inset= c(0.1, 0.1))
  
 ##########
 # 4 plots showing pH, vegetation cover, canopy cover, and temperature across transects
@@ -106,12 +101,12 @@ EUcolors <- c("EU 10" = "#d73027", "EU 52" = "#4575b4", "EU 53N" = "#fc8d59",
 
 # pH
 pHEUMed_plot <- ggplot() + 
-  geom_line(data=pHCanVegMedEU[1:10,], aes(x=Meter, y=medpHEu, group = EU), color = "#d73027", size= 1.5) +
-  geom_line(data=pHCanVegMedEU[11:20,], aes(x=Meter, y=medpHEu, group = EU), color = "#4575b4", size= 1.5) +
-  geom_line(data=pHCanVegMedEU[21:30,], aes(x=Meter, y=medpHEu, group = EU), color = "#fc8d59", size= 1.5) +
-  geom_line(data=pHCanVegMedEU[31:40,], aes(x=Meter, y=medpHEu, group = EU), color = "#91bfdb", size= 1.5) +
-  geom_line(data=pHCanVegMedEU[41:50,], aes(x=Meter, y=medpHEu, group = EU), color = "#fee090", size= 1.5) +
-  geom_line(data=pHCanVegMedEU[51:60,], aes(x=Meter, y=medpHEu, group = EU), color = "#e0f3f8", size= 1.5) +
+  geom_line(data=pHCanVegMedEU[1:10,], aes(x=Meter, y=meanpHEu, group = EU), color = "#d73027", size= 1.5) +
+  geom_line(data=pHCanVegMedEU[11:20,], aes(x=Meter, y=meanpHEu, group = EU), color = "#4575b4", size= 1.5) +
+  geom_line(data=pHCanVegMedEU[21:30,], aes(x=Meter, y=meanpHEu, group = EU), color = "#fc8d59", size= 1.5) +
+  geom_line(data=pHCanVegMedEU[31:40,], aes(x=Meter, y=meanpHEu, group = EU), color = "#91bfdb", size= 1.5) +
+  geom_line(data=pHCanVegMedEU[41:50,], aes(x=Meter, y=meanpHEu, group = EU), color = "#fee090", size= 1.5) +
+  geom_line(data=pHCanVegMedEU[51:60,], aes(x=Meter, y=meanpHEu, group = EU), color = "#e0f3f8", size= 1.5) +
   theme_bw() + ylim(3, 7) +
   scale_x_continuous("Transect Meter", breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
   ggtitle("Mean pH across transect") + geom_vline(xintercept = 50, linetype= "dashed", color= "grey") +
@@ -151,10 +146,11 @@ vegEU_plot
 
 ##### PLOT ALL TOGETHER #####
 quartz()
-require(gridExtra)
 grid.arrange(pHEUMed_plot, CanEU_plot, vegEU_plot, ncol=3)
 
-####### CANOPY COVER "COLOR BARS" #########
+############### CANOPY COVER "COLOR BARS" ###################
+############################################################
+
 # Aug 9, 2022 (based on tutorial here: https://ggplot2.tidyverse.org/reference/guide_colourbar.html)
 ########################
 df <- expand.grid(X1 = 1:10, X2 = 1:10)
@@ -170,6 +166,9 @@ canCoverVec10 <- pHCanVegMedEU$avgCanopyEu[1:10] #add  just the canopy cover dat
 canEU10_df$value <- rep(canCoverVec10, 10) #add canopy cover data
 # View(canEU10_df) #Now, each row in this 10 x 10 grid should be the same, and should be the mean canopy
 # cover across transect 10
+canEU10_df$value #since only the first row is what we are using for the color bar, we will add in some 
+# high and low numbers in another row so that color bar goes from 0 to 100 percent canopy cover
+canEU10_df$value[1:10] <- c(rep(0,5), rep(100,5))
 
 canCoverEU_10_grid <- ggplot(canEU10_df, aes(X, Y)) + geom_tile(aes(fill = value)) + 
   ggtitle("Mean Canopy Cover EU 10") +
@@ -194,6 +193,9 @@ canCoverVec53S <- pHCanVegMedEU$avgCanopyEu[31:40] #add  just the canopy cover d
 canEU53S_df$value <- rep(canCoverVec53S, 10) #add canopy cover data
 # View(canEU53S_df) #Now, each row in this 10 x 10 grid should be the same, and should be the mean canopy
 # cover across transect 53S
+canEU53S_df$value #since only the first row is what we are using for the color bar, we will add in some 
+# high and low numbers in another row so that color bar goes from 0 to 100 percent canopy cover
+canEU53S_df$value[1:10] <- c(rep(0,5), rep(100,5))
 
 canCoverEU_53S_grid <- ggplot(canEU53S_df, aes(X, Y)) + geom_tile(aes(fill = value)) + 
   ggtitle("Mean Canopy Cover EU 53S") +
@@ -280,9 +282,6 @@ quartz()
 canCoverEU_52_grid + guides(fill = guide_colourbar(ticks=FALSE, label.position = "left", nbin = 100, title= "Canopy cover",
                                                    direction= "vertical", barwidth = 1.5, barheight = 6)) + 
   theme_bw()
-
-
-
 
 
 # save pH, canopy cover, and vegetation data for transects
